e4427f2e47bd5cfe684fd5b203c51d0da141e885
Jorge <jorge.barbosa@indt.org.br>
2021-02-08 21:06:40 -0400

    chapter 7 - franc-ly speaking

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -2,7 +2,6 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 public class DollarTest {
@@ -17,6 +16,13 @@ public class DollarTest {
     public void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
+    }
+
+    @Test
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
     }
 }
 
